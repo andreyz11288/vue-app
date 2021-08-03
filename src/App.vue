99d2edd57660ff467002434d7a9bcb26d450c4d1
andreyz11288 <andreyz11288@gmail.com>
2021-08-03 16:16:31 +0300

    2

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+   <Button  @click="increments">Click me</Button>
+   <DirectiveTest/>
+   <StarRating :rating="4.5"/>
+  </div>
+</template>
+
+<script>
+
+import Button from "./components/Button";
+import DirectiveTest from "./components/DirectivesTest.vue";
+import StarRating from "./components/StarRating.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Button,
+    DirectiveTest,
+    StarRating
+  },
+  data(){
+    return{
+      amountOfClicks: 0
+    }
+  },
+  computed:{
+   title(){
+     return ` Amount of click ${this.amountOfClicks}`
+   }
+  },
+  methods:{
+    increments(){
+      this.amountOfClicks += 1
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DirectivesTest.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if='isVisible'>
+        <h1>Directive</h1>
+        <ul>
+            <template v-for="fruit in fruits" >
+                <li v-if="fruit !== 'mango'" :key="fruit" >{{fruit}}</li>
+            </template>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DirectiveTest',
+        data(){
+            return{
+                isVisible:true,
+            fruits:['banana', 'apple', 'orange', 'mango']
+                }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
